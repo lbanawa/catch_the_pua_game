5e43ba49bc6a6279207a25b741d2ec20b3a16b7f
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-03-26 15:05:50 -0400

    view controller initial variables/ objects
    added the label and image objects from main.storyboard to ViewController

--- CatchThePuaGame/ViewController.swift
+++ CatchThePuaGame/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var pua1: UIImageView!
+    @IBOutlet weak var pua2: UIImageView!
+    @IBOutlet weak var pua3: UIImageView!
+    @IBOutlet weak var pua4: UIImageView!
+    @IBOutlet weak var pua5: UIImageView!
+    @IBOutlet weak var pua6: UIImageView!
+    @IBOutlet weak var pua7: UIImageView!
+    @IBOutlet weak var pua8: UIImageView!
+    @IBOutlet weak var pua9: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
